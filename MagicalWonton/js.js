7b096dcf611bddee4c5e4eb815bbd2003f4de418
joliebaskett <jbaskett@horizon.csueastbay.edu>
2017-11-21 17:24:50 -0800

    Add files via upload

---
+++ MagicalWonton/js.js
@@ -0,0 +1,9 @@
+    
+    
+    
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script>
+    $(document).ready(function() {
+      $('#onward3').hide().delay(2000).fadeIn(1000);
+});
+    </script>
