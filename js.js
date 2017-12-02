a87bb986dd0e64eb37930457cb573ad8681e4888
joliebaskett <jbaskett@horizon.csueastbay.edu>
2017-12-01 16:32:58 -0800

    Add files via upload

--- js.js
+++ js.js
@@ -4,7 +4,7 @@
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script>
     $(document).ready(function() {
-      $('#onward3').hide().delay(2000).fadeIn(1000);
+      $('#onward3').hide().delay(5000).fadeIn(4000);
 });
     </script>
     
